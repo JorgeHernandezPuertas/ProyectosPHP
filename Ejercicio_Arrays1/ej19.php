af4a32dfafef9c60caa42b88c684c0b6f0947228
JorgeHernandezPuertas <jherpue553@g.educaand.es>
2023-09-27 17:58:50 +0200

    Termino la relacion de arrays

--- Ejercicio_Arrays1/ej19.php
+++ Ejercicio_Arrays1/ej19.php
@@ -7,8 +7,23 @@
 </head>
 <body>
     <?php
-        $ciudades = array("madrid" => array("amigo1" => array("nombre" => "Pedro", "edad" => 32, "telefono" => "91-999.99.99"), "amigo2" => array("nombre" => "Manolo", "edad" => 36, "telefono" => "91-949.99.99"),
-        "amigo3" => array("nombre" => "Manolo", "edad" => 36, "telefono" => "91-949.99.99")));
+        $ciudades = array("Madrid" => array("amigo1" => array("nombre" => "Pedro", "edad" => 32, "telefono" => "91-999.99.99"), "amigo2" => array("nombre" => "Manolo", "edad" => 36, "telefono" => "91-949.99.99"),
+        "amigo3" => array("nombre" => "Cristobal", "edad" => 22, "telefono" => "2134")), "Barcelona" => array("amigo1" => array("nombre" => "Pablo", "edad" => 24, "telefono" => "1241234"), "amigo2" => array("nombre" => "Dario", "edad" => 15, "telefono" => "73456234")), 
+        "Toledo" => array("amigo1" => array("nombre" => "Pepito", "edad" => 22, "telefono" => "746345"), "amigo2" => array("nombre" => "Juan", "edad" => 29, "telefono" => "83456"),
+        "amigo3" => array("nombre" => "Cristina", "edad" => 23, "telefono" => "66437345")));
+
+        foreach($ciudades as $ciudad => $amigos){
+            echo "<p>En $ciudad:<ol>";
+            foreach($amigos as $amigo => $datos){
+                echo "<li>";
+                foreach($datos as $dato => $valor){
+                    echo "$dato: $valor ";
+                }
+                echo "</li>";
+            }
+            echo "</ol></p>";
+        }
+
     ?>
 </body>
 </html>
