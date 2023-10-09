7c2ad585e60742834dec8c4104af03010be5d8f2
JorgeHernandezPuertas <jherpue553@g.educaand.es>
2023-10-09 20:07:10 +0200

    Termino el ejercicio fecha1

--- fechas/fecha1.php
+++ fechas/fecha1.php
@@ -3,7 +3,7 @@ if (isset($_POST["btnEnviar"])) {
     // Compruebo la fecha1
     $fecha1 = trim($_POST["fecha1"]);
     $error_fecha1_vacia = $fecha1 == "";
-    $error_fecha1_formato = preg_match("/^\d{2}\/\d{2}\/\d{4}$/", $fecha1);
+    $error_fecha1_formato = !preg_match("/^\d{2}\/\d{2}\/\d{4}$/", $fecha1);
     $error_fecha1_posible = false;
     if (!$error_fecha1_formato) {
         $array_fecha1 = explode("/", $fecha1);
@@ -13,8 +13,8 @@ if (isset($_POST["btnEnviar"])) {
 
     // Compruebo la fecha2
     $fecha2 = trim($_POST["fecha2"]);
-    $error_fecha2_vacia = $fecha1 == "";
-    $error_fecha2_formato = preg_match("/^\d{2}\/\d{2}\/\d{4}$/", $fecha2);
+    $error_fecha2_vacia = $fecha2 == "";
+    $error_fecha2_formato = !preg_match("/^\d{2}\/\d{2}\/\d{4}$/", $fecha2);
     $error_fecha2_posible = false;
     if (!$error_fecha2_formato) {
         $array_fecha2 = explode("/", $fecha2);
@@ -37,23 +37,77 @@ if (isset($_POST["btnEnviar"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fecha1</title>
+    <style>
+        .error {
+            color: red
+        }
+
+        .azul {
+            background: lightblue;
+        }
+
+        .verde {
+            background: lightgreen;
+        }
+
+        div {
+            padding: 1em;
+            border: 1px solid black;
+            margin-bottom: 1em;
+        }
+    </style>
 </head>
-
 <body>
-    <h1>Fechas-Formulario</h1>
-    <form action="fecha1.php" method="post"></form>
-    <p>
-        <label for="fecha1">Introduzca una fecha: (DD/MM/YYYY)</label>
-        <input type="text" name="fecha1" id="fecha1" value="<?php if (isset($_POST["btnEnviar"]) && !$error_fecha1) echo $_POST["fecha1"] ?>" />
-    </p>
-    <p>
-        <label for="fecha2">Introduzca una fecha: (DD/MM/YYYY)</label>
-        <input type="text" name="fecha2" id="fecha2" value="<?php if (isset($_POST["btnEnviar"]) && !$error_fecha1) echo $_POST["fecha2"] ?>" />
-    </p>
-    <p>
-        <input type="submit" name="btnEnviar" value="Calcular" />
-    </p>
-
+    <div class="azul">
+        <h1>Fechas-Formulario</h1>
+        <form action="fecha1.php" method="post">
+        <p>
+            <label for="fecha1">Introduzca una fecha: (DD/MM/YYYY)</label>
+            <input type="text" name="fecha1" id="fecha1" value="<?php if (isset($_POST["btnEnviar"]) && !$error_fecha1) echo $_POST["fecha1"] ?>" />
+            <?php
+            if (isset($_POST["btnEnviar"]) && $error_fecha1_vacia) {
+                echo "<span class='error'> *Campo vacio* </span>";
+            } else if (isset($_POST["btnEnviar"]) && $error_fecha1_formato) {
+                print "<span class='error'> *Error en el formato de la fecha* </span>";
+            } else if (isset($_POST["btnEnviar"]) && $error_fecha1_posible) {
+                print "<span class='error'> *Esa fecha no existe* </span>";
+            }
+            ?>
+        </p>
+        <p>
+            <label for="fecha2">Introduzca una fecha: (DD/MM/YYYY)</label>
+            <input type="text" name="fecha2" id="fecha2" value="<?php if (isset($_POST["btnEnviar"]) && !$error_fecha2) echo $_POST["fecha2"] ?>" />
+            <?php
+            if (isset($_POST["btnEnviar"]) && $error_fecha2_vacia) {
+                print "<span class='error'> *Campo vacio* </span>";
+            } else if (isset($_POST["btnEnviar"]) && $error_fecha2_formato) {
+                print "<span class='error'> *Error en el formato de la fecha* </span>";
+            } else if (isset($_POST["btnEnviar"]) && $error_fecha2_posible) {
+                print "<span class='error'> *Esa fecha no existe* </span>";
+            }
+            ?>
+        </p>
+        <p>
+            <input type="submit" name="btnEnviar" value="Calcular" />
+        </p>
+        </form>
+    </div>
+    <?php
+    if (isset($_POST["btnEnviar"]) && !$errores){
+        
+        $segundos1 = strtotime($array_fecha1[1]."/".$array_fecha1[0]."/".$array_fecha1[2]);
+        $segundos2 = strtotime($array_fecha2[1]."/".$array_fecha2[0]."/".$array_fecha2[2]);
+        $diferenciaSegundos = abs($segundos1 - $segundos2);
+        $diferenciaDias = floor($diferenciaSegundos / (3600 * 24));
+        ?>
+        <div class="verde">
+        <h1>Fechas - Respuesta</h1>
+        <?php
+        print "<p>La diferencia en días entre las dos fechas es de $diferenciaDias días.</p>";
+        ?>
+        </div>
+        <?php
+    }
+    ?>
 </body>
-
 </html>
