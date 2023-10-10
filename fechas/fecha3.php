efcf2165b515eefa0d0460ce8ada1d98bddf8c43
JorgeHernandezPuertas <jherpue553@g.educaand.es>
2023-10-10 19:33:01 +0200

    termino la fecha3

--- fechas/fecha3.php
+++ fechas/fecha3.php
@@ -2,18 +2,21 @@
 if (isset($_POST["btnEnviar"])) {
     
     // Como lo he hecho con date el único error es que esté el campo sin rellenar
-    $error_fecha1_vacia = $_POST["fecha1"] = "";
-    $error_fecha2_vacia = $_POST["fecha1"] = "";
+    
+    $error_fecha1_vacia = $_POST["fecha1"] == "";
+    $error_fecha2_vacia = $_POST["fecha2"] == "";
     $errores = $error_fecha1_vacia || $error_fecha2_vacia;
 
     if (!$error_fecha1_vacia){
         $array_fecha1 = explode("-", $_POST["fecha1"]);
-        $fecha1 = date("m/d/Y", strtotime($array_fecha1[1]."/".$array_fecha1[2]."/".$array_fecha1[0]));
+        $segundos1 = strtotime($array_fecha1[1]."/".$array_fecha1[2]."/".$array_fecha1[0]);
+        $fecha1 = date("m/d/Y", $segundos1);
     }
     
     if (!$error_fecha2_vacia){
         $array_fecha2 = explode("-", $_POST["fecha2"]);
-        $fecha2 = date("m/d/Y", strtotime($array_fecha2[1]."/".$array_fecha2[2]."/".$array_fecha2[0]));
+        $segundos2 = strtotime($array_fecha2[1]."/".$array_fecha2[2]."/".$array_fecha2[0]);
+        $fecha2 = date("m/d/Y", $segundos2);
     }
 
 }
@@ -55,7 +58,7 @@ if (isset($_POST["btnEnviar"])) {
         <form action="fecha3.php" method="post">
         <p>
             <label for="fecha1">Introduzca una fecha: </label>
-            <input type="date" name="fecha1" />
+            <input type="date" name="fecha1" value="<?php if (isset($_POST["btnEnviar"])) print $_POST["fecha1"] ?>" />
             <?php
             if (isset($_POST["btnEnviar"]) && $error_fecha1_vacia){
                 print "<span class='error'> *Campo obligatorio* </span>";
@@ -64,9 +67,9 @@ if (isset($_POST["btnEnviar"])) {
         </p>
         <p>
         <label for="fecha2">Introduzca una fecha: </label>
-            <input type="date" name="fecha2" />
+            <input type="date" name="fecha2" value="<?php if (isset($_POST["btnEnviar"])) print $_POST["fecha2"] ?>" />
             <?php
-            if (isset($_POST["btnEnviar"]) && $error_fecha1_vacia){
+            if (isset($_POST["btnEnviar"]) && $error_fecha2_vacia){
                 print "<span class='error'> *Campo obligatorio* </span>";
             }
             ?>
@@ -78,9 +81,6 @@ if (isset($_POST["btnEnviar"])) {
     </div>
     <?php
     if (isset($_POST["btnEnviar"]) && !$errores){
-        
-        $segundos1 = strtotime($_POST["mes1"]."/".$_POST["dia1"]."/".$_POST["anio1"]);
-        $segundos2 = strtotime($_POST["mes2"]."/".$_POST["dia2"]."/".$_POST["anio2"]);
         $diferenciaSegundos = abs($segundos1 - $segundos2);
         $diferenciaDias = floor($diferenciaSegundos / (3600 * 24));
         ?>
