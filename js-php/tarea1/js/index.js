5f4247982d47409a38babbd18c5f34ac6aa2f71a
jorge <jherpue553@g.educaand.es>
2024-03-11 09:51:09 +0100

    Termino la tarea1

--- js-php/tarea1/js/index.js
+++ js-php/tarea1/js/index.js
@@ -21,7 +21,7 @@ function obtener_productos () {
         tabla_productos += `<td><button class='enlace' onclick='obtener_detalles("${tupla.cod}")'>  ${tupla.cod}  </button></td>`
         tabla_productos += "<td>" + tupla["nombre_corto"] + "</td>"
         tabla_productos += "<td>" + tupla["PVP"] + "</td>"
-        tabla_productos += `<td><button class='enlace' onclick='editarProducto("${tupla.cod}")'>Editar</button> - <button class='enlace' onclick='eliminarProducto("${tupla.cod}")'>Borrar</button></td>`
+        tabla_productos += `<td><button class='enlace' onclick='crearFormEditar("${tupla.cod}")'>Editar</button> - <button class='enlace' onclick='eliminarProducto("${tupla.cod}")'>Borrar</button></td>`
         tabla_productos += "</tr>"
       });
       tabla_productos += "</table>"
@@ -63,15 +63,15 @@ async function nuevoProducto () {
                       <span class='error'></span>
                     </p>
                     <p>
-                      <label for='nombre_cor'><strong>Nombre corto: </strong></label><input type='text' name='nombre_cor' id='nombre_cor' required/>
+                      <label for='nombre_corto'><strong>Nombre corto: </strong></label><input type='text' name='nombre_corto' id='nombre_corto' required/>
                       <span class='error'></span>
                     </p>
                     <p>
-                      <label for='desc'><strong>Descripción: </strong></label><textarea name='desc' id='desc' required ></textarea>
+                      <label for='descripcion'><strong>Descripción: </strong></label><textarea name='descripcion' id='descripcion' required ></textarea>
                       <span class='error'></span>
                     </p>
                     <p>
-                      <label for='pvp'><strong>PVP: </strong></label><input type='number' name='pvp' id='pvp' required />
+                      <label for='PVP'><strong>PVP: </strong></label><input type='number' name='PVP' id='PVP' required />
                       <span class='error'></span>
                     </p>
   `
@@ -96,14 +96,28 @@ async function nuevoProducto () {
       formulario += `<option value='${tupla.cod}' >${tupla.nombre}</option>`
     })
     formulario += `</select></p>
-                  <p><button type='submit'>Añadir</button> <button onClick='resetHtml()'>Volver</button></p>
+                  <p><button type='submit'>Añadir</button> <button onClick='event.preventDefault(); resetHtml()'>Volver</button></p>
                   </form>`
     $("#acciones").html(formulario)
   }
 }
 
+async function comprobarRepetidoExcluido (tabla, columna, valor, colId, id) {
+  const respuesta = await $.ajax({
+    url: encodeURI(`${DIR_SERV}/repetido/${tabla}/${columna}/${valor}/${colId}/${id}`),
+    dataType: 'json',
+    type: 'get'
+  })
+
+  if (respuesta.error) return true
+
+  if (respuesta.mensaje_error) return true
+
+  return respuesta.repetido
+}
+
 async function comprobarRepetido (tabla, columna, valor) {
-  const respuesta = $.ajax({
+  const respuesta = await $.ajax({
     url: encodeURI(`${DIR_SERV}/repetido/${tabla}/${columna}/${valor}`),
     dataType: 'json',
     type: 'get'
@@ -118,15 +132,39 @@ async function comprobarRepetido (tabla, columna, valor) {
 
 async function crearProducto (event) {
   event.preventDefault()
-  const form = event.target
+  const FORM = event.target
 
-  let codRepetido = await comprobarRepetido('producto', 'cod', form.cod.value)
-  let nombreCorRepetido = await comprobarRepetido('producto', 'nombre_corto', form.nombre_cor.value)
+
+  let codRepetido = await comprobarRepetido('producto', 'cod', FORM.cod.value)
+  let nombreCorRepetido = await comprobarRepetido('producto', 'nombre_corto', FORM.nombre_corto.value)
   if (codRepetido) {
-    $(form.cod).siblings("span.error").html('* Código repetido *')
+    $(FORM.cod).siblings("span.error").html('* Código repetido *')
   }
   if (nombreCorRepetido) {
-    $(form.nombre_cor).siblings("span.error").html('* Código repetido *')
+    $(FORM.nombre_corto).siblings("span.error").html('* Nombre corto repetido *')
+  }
+
+  if (!codRepetido && !nombreCorRepetido) {
+    // Mapeo el formulario
+    const FORM_DATA = new FormData(FORM)
+    const ENTRIES = FORM_DATA.entries()
+    let datos = {}
+
+    for (var tupla of ENTRIES) {
+      datos[tupla[0]] = tupla[1]
+    }
+    // Inserto el producto válido
+    await $.ajax({
+      url: encodeURI(`${DIR_SERV}/producto/insertar`),
+      dataType: 'json',
+      type: 'post',
+      data: datos
+    })
+
+    // Pongo el mensaje
+    $("#acciones").html(`<p class='mensaje'>El producto ha sido creado con éxito</p>`)
+    // Refresco la página
+    obtener_productos()
   }
 }
 
@@ -163,10 +201,109 @@ async function obtener_detalles (cod) {
 
       detalles += `<p><button onclick='resetHtml()' >volver</button></p>`
 
-
+      // Pongo el mensaje
       $("#acciones").html(detalles)
     }
   })
+}
+
+async function crearFormEditar (cod) {
+  resetHtml()
+  // Obtengo los datos actuales
+  const respuestaDatos = await $.ajax({
+    url: DIR_SERV + `/productos/${cod}`,
+    dataType: "json",
+    type: "GET",
+  })
+
+  // Si devuelve error lo pongo
+  if (respuesta.error) { // Error del propio jquery
+    $("#acciones").html(respuesta.error)
+  } else if (respuesta.mensaje_error) {
+    $("#acciones").html(respuesta.mensaje_error)
+  } else {
+    const producto = respuestaDatos.producto
+    let formulario = `<form onsubmit='editarProducto(event)' method='post' action='.'>
+                    <h2>Editando el producto ${cod}</h2>
+                    <p>
+                      <label for='nombre'><strong>Nombre: </strong></label><input type='text' name='nombre' id='nombre' value='${producto.nombre}' />
+                      <span class='error'></span>
+                    </p>
+                    <p>
+                      <label for='nombre_corto'><strong>Nombre corto: </strong></label><input type='text' name='nombre_corto' id='nombre_corto' value='${producto.nombre_corto}' required/>
+                      <span class='error'></span>
+                    </p>
+                    <p>
+                      <label for='descripcion'><strong>Descripción: </strong></label><textarea name='descripcion' id='descripcion' required >${producto.descripcion}</textarea>
+                      <span class='error'></span>
+                    </p>
+                    <p>
+                      <label for='PVP'><strong>PVP: </strong></label><input type='number' name='PVP' id='PVP' value='${producto.PVP}' required />
+                      <span class='error'></span>
+                    </p>
+  `
+    // añado las familias del select
+    const respuesta = await $.ajax({
+      url: DIR_SERV + "/familias",
+      dataType: "json",
+      type: "GET",
+    })
+
+    // Si devuelve error lo pongo
+    if (respuesta.error) { // Error del propio jquery
+      $("#acciones").html(respuesta.error)
+    } else if (respuesta.mensaje_error) {
+      $("#acciones").html(respuesta.mensaje_error)
+    } else {
+      // Si no devuelve error, pongo el select con las familias
+      formulario += `<p>
+                  <label for='familia'><strong>Familia: </strong></label>
+                  <select name='familia' id='familia'>`
+      $.each(respuesta.familias, function (key, tupla) {
+        formulario += tupla.cod == producto.familia ? `<option value='${tupla.cod}' selected >${tupla.nombre}</option>` : `<option value='${tupla.cod}' >${tupla.nombre}</option>`
+      })
+      formulario += `</select></p>
+                  <p><input type='hidden' name='cod' value='${cod}' /><button type='submit'>Editar</button> <button onClick='event.preventDefault(); resetHtml()'>Volver</button></p>
+                  </form>`
+      $("#acciones").html(formulario)
+    }
+  }
+}
+
+async function editarProducto (event) {
+  event.preventDefault()
+
+  const FORM = event.target
+  const cod = FORM.cod.value
+
+  let nombreCorRepetido = await comprobarRepetidoExcluido('producto', 'nombre_corto', FORM.nombre_corto.value, 'cod', cod)
+  if (nombreCorRepetido) {
+    $(FORM.nombre_corto).siblings("span.error").html('* Nombre corto repetido *')
+  }
+
+  if (!nombreCorRepetido) {
+    // Mapeo el formulario
+    const FORM_DATA = new FormData(FORM)
+    const ENTRIES = FORM_DATA.entries()
+    let datos = {}
+
+    for (var tupla of ENTRIES) {
+      if (tupla[0] != "cod")
+        datos[tupla[0]] = tupla[1]
+    }
+    // Inserto el producto válido
+    await $.ajax({
+      url: encodeURI(`${DIR_SERV}/producto/actualizar/${cod}`),
+      dataType: 'json',
+      type: 'put',
+      data: datos
+    })
+
+    // Pongo el mensaje
+    $("#acciones").html(`<p class='mensaje'>El producto ha sido editado con éxito</p>`)
+    // Refresco la página
+    obtener_productos()
+  }
 }
 
 function resetHtml () {
